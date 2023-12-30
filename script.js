0d4a348ac4b225e7475db5022459e7ace54063ab
CyanFaux <santafox26@gmail.com>
2023-12-30 17:26:03 -0500

    shift in focus

--- script.js
+++ script.js
@@ -1,198 +1,86 @@
-const playingField = [
-  [
-    "a13",
-    "b13",
-    "c13",
-    "d13",
-    "e13",
-    "f13",
-    "g13",
-    "h13",
-    "i13",
-    "j13",
-    "k13",
-    "l13",
-    "m13",
-  ],
-  [
-    "a12",
-    "b12",
-    "c12",
-    "d12",
-    "e12",
-    "f12",
-    "g12",
-    "h12",
-    "i12",
-    "j12",
-    "k12",
-    "l12",
-    "m12",
-  ],
-  [
-    "a11",
-    "b11",
-    "c11",
-    "d11",
-    "e11",
-    "f11",
-    "g11",
-    "h11",
-    "i11",
-    "j11",
-    "k11",
-    "l11",
-    "m11",
-  ],
-  [
-    "a10",
-    "b10",
-    "c10",
-    "d10",
-    "e10",
-    "f10",
-    "g10",
-    "h10",
-    "i10",
-    "j10",
-    "k10",
-    "l10",
-    "m10",
-  ],
-  [
-    "a9",
-    "b9",
-    "c9",
-    "d9",
-    "e9",
-    "f9",
-    "g9",
-    "h9",
-    "i9",
-    "j9",
-    "k9",
-    "l9",
-    "m9",
-  ],
-  [
-    "a8",
-    "b8",
-    "c8",
-    "d8",
-    "e8",
-    "f8",
-    "g8",
-    "h8",
-    "i8",
-    "j8",
-    "k8",
-    "l8",
-    "m8",
-  ],
-  [
-    "a7",
-    "b7",
-    "c7",
-    "d7",
-    "e7",
-    "f7",
-    "g7",
-    "h7",
-    "i7",
-    "j7",
-    "k7",
-    "l7",
-    "m7",
-  ],
-  [
-    "a6",
-    "b6",
-    "c6",
-    "d6",
-    "e6",
-    "f6",
-    "g6",
-    "h6",
-    "i6",
-    "j6",
-    "k6",
-    "l6",
-    "m6",
-  ],
-  [
-    "a5",
-    "b5",
-    "c5",
-    "d5",
-    "e5",
-    "f5",
-    "g5",
-    "h5",
-    "i5",
-    "j5",
-    "k5",
-    "l5",
-    "m5",
-  ],
-  [
-    "a4",
-    "b4",
-    "c4",
-    "d4",
-    "e4",
-    "f4",
-    "g4",
-    "h4",
-    "i4",
-    "j4",
-    "k4",
-    "l4",
-    "m4",
-  ],
-  [
-    "a3",
-    "b3",
-    "c3",
-    "d3",
-    "e3",
-    "f3",
-    "g3",
-    "h3",
-    "i3",
-    "j3",
-    "k3",
-    "l3",
-    "m3",
-  ],
-  [
-    "a2",
-    "b2",
-    "c2",
-    "d2",
-    "e2",
-    "f2",
-    "g2",
-    "h2",
-    "i2",
-    "j2",
-    "k2",
-    "l2",
-    "m2",
-  ],
-  [
-    "a1",
-    "b1",
-    "c1",
-    "d1",
-    "e1",
-    "f1",
-    "g1",
-    "h1",
-    "i1",
-    "j1",
-    "k1",
-    "l1",
-    "m1",
-  ],
-];
+/* sets fieldDiv to "field-div" */
+const fieldDiv = document.getElementById("field-div");
 
+/* starts rows from the bottom */
+for (let row = 13; row >= 1; row--) {
+/* starts columns from the left */
+  for (let col = 1; col <= 13; col++) {
+    /* sets the cell content to "a1", "a2", "a3", etc */
+    const cellContent = `${String.fromCharCode(96 + col)}${row}`;
+    /* creates a div referred to as fieldTile */
+    const fieldTile = document.createElement("div");
+    /* set id based on cell string */
+    fieldTile.id = cellContent;
+    /* add class */
+    fieldTile.classList.add("field-tile-class");
+    /* adds div */
+    fieldDiv.appendChild(fieldTile);
+    /* adds text based on playingField array */
+    fieldTile.textContent = cellContent;
+  }
+}
+/* declare that allCells refers to cells with .field-tile-class */
+const allCells = document.querySelectorAll('.field-tile-class');
+
+const towerWalls = [
+  "i13",
+  "j13",
+  "i12",
+  "j12",
+  "i11",
+  "j11",
+  "i10",
+  "j10",
+  "i9",
+  "j9",
+  "i8",
+  "j8",
+  "j7",
+  "i7",
+  "j6",
+  "i6",
+  "i5",
+  "j5",
+  "i4",
+  "j4",
+  "d1",
+  "d2",
+  "d3",
+  "d4",
+  "d5",
+  "d6",
+  "d7",
+  "d8",
+  "d9",
+  "d10",
+  "e1",
+  "e2",
+  "e3",
+  "e4",
+  "e5",
+  "e6",
+  "e7",
+  "e8",
+  "e9",
+  "e10",
+]
+
+allCells.forEach(cell => {
+  /* equates placeholder variable with loop result */
+  const cellIdValue = cell.id
+/* defines criteria for towerWalls */
+  if (towerWalls.includes(cellIdValue)) {
+    /* changes class to towerWalls */
+    cell.classList.add('towerWalls');
+  }
+
+});
+/* define createTower function */ 
+const createTower = document.querySelectorAll(".towerWalls")
+/* createTower changes background color and class of tile */
+createTower.forEach(cell => {
+  cell.addEventListener("click", () => {
+    cell.style.backgroundColor = "blue";
+    cell.classList.add("squareTower-class")
+  })
+})
--- square.js
+++ square.js
@@ -1,13 +1,200 @@
-const fieldDiv = document.getElementById("field-div");
-
-for (let row = 0; row < playingField.length; row++) {
-  for (let col = 0; col < playingField[row].length; col++) {
-    const fieldTile = document.createElement("div");
-    fieldTile.textContent = playingField[row][col];
-    fieldTile.classList.add("field-tile-class");
-    fieldDiv.appendChild(fieldTile);
-  }
-}
+const playingField = [
+  [
+    "a13",
+    "b13",
+    "c13",
+    "d13",
+    "e13",
+    "f13",
+    "g13",
+    "h13",
+    "i13",
+    "j13",
+    "k13",
+    "l13",
+    "m13",
+  ],
+  [
+    "a12",
+    "b12",
+    "c12",
+    "d12",
+    "e12",
+    "f12",
+    "g12",
+    "h12",
+    "i12",
+    "j12",
+    "k12",
+    "l12",
+    "m12",
+  ],
+  [
+    "a11",
+    "b11",
+    "c11",
+    "d11",
+    "e11",
+    "f11",
+    "g11",
+    "h11",
+    "i11",
+    "j11",
+    "k11",
+    "l11",
+    "m11",
+  ],
+  [
+    "a10",
+    "b10",
+    "c10",
+    "d10",
+    "e10",
+    "f10",
+    "g10",
+    "h10",
+    "i10",
+    "j10",
+    "k10",
+    "l10",
+    "m10",
+  ],
+  [
+    "a9",
+    "b9",
+    "c9",
+    "d9",
+    "e9",
+    "f9",
+    "g9",
+    "h9",
+    "i9",
+    "j9",
+    "k9",
+    "l9",
+    "m9",
+  ],
+  [
+    "a8",
+    "b8",
+    "c8",
+    "d8",
+    "e8",
+    "f8",
+    "g8",
+    "h8",
+    "i8",
+    "j8",
+    "k8",
+    "l8",
+    "m8",
+  ],
+  [
+    "a7",
+    "b7",
+    "c7",
+    "d7",
+    "e7",
+    "f7",
+    "g7",
+    "h7",
+    "i7",
+    "j7",
+    "k7",
+    "l7",
+    "m7",
+  ],
+  [
+    "a6",
+    "b6",
+    "c6",
+    "d6",
+    "e6",
+    "f6",
+    "g6",
+    "h6",
+    "i6",
+    "j6",
+    "k6",
+    "l6",
+    "m6",
+  ],
+  [
+    "a5",
+    "b5",
+    "c5",
+    "d5",
+    "e5",
+    "f5",
+    "g5",
+    "h5",
+    "i5",
+    "j5",
+    "k5",
+    "l5",
+    "m5",
+  ],
+  [
+    "a4",
+    "b4",
+    "c4",
+    "d4",
+    "e4",
+    "f4",
+    "g4",
+    "h4",
+    "i4",
+    "j4",
+    "k4",
+    "l4",
+    "m4",
+  ],
+  [
+    "a3",
+    "b3",
+    "c3",
+    "d3",
+    "e3",
+    "f3",
+    "g3",
+    "h3",
+    "i3",
+    "j3",
+    "k3",
+    "l3",
+    "m3",
+  ],
+  [
+    "a2",
+    "b2",
+    "c2",
+    "d2",
+    "e2",
+    "f2",
+    "g2",
+    "h2",
+    "i2",
+    "j2",
+    "k2",
+    "l2",
+    "m2",
+  ],
+  [
+    "a1",
+    "b1",
+    "c1",
+    "d1",
+    "e1",
+    "f1",
+    "g1",
+    "h1",
+    "i1",
+    "j1",
+    "k1",
+    "l1",
+    "m1",
+  ],
+];
 
 const allCells = fieldDiv.querySelectorAll(".field-tile-class");
 
@@ -151,7 +338,6 @@ allCells.forEach((cell) => {
     }
   }
 });
-
 const cellWalls = [
   "i13",
   "j13",
@@ -220,25 +406,9 @@ allCells.forEach((cell) => {
     console.log("blueCellId:", blueCellId);
     cell.addEventListener("click", () => {
       cell.style.backgroundColor = "blue";
-
-      const borderPattern = getBorderPattern(blueCellId);
-
-      borderPattern.forEach((targetCellId) => {
-        const adjacentCell = findCellById(targetCellId);
-
-        if (adjacentCell) {
-      const existingBorderStyle = getComputedStyle(adjacentCell).border;
-      const.borderSides = existingBorderStyle.split('');
-        for (let i = 0; i < borderSides.length; i++) {
-          if (borderSides[i] === "none" || borderSides[i] === "0px") {
-
-    borderSides[i] = "2px solid red";
   }
-        }
+)}
       });
-    });
-  }
-});
 
 // Define a function to determine the border pattern relative to the blue cell's position
 function getBorderPattern(blueCellId) {
@@ -277,7 +447,7 @@ const cellGroups = [
     ["i5", "i4", "j5", "j4"],
 ]
 
-function areAllCellsBlue(group) {
+/* function areAllCellsBlue(group) {
     for (const cellId of group) {
         const cell = document.querySelector(`.field-tile-class:contains("${cellId}")`)
         if (!cell) {
@@ -292,7 +462,7 @@ function areAllCellsBlue(group) {
     console.log("cells merge!")
     return true;
    
-}
+} */
 
 const removeCellBoxShadow = ["d1", "e1"];
 
